40693c587b79083469720fa653a838f7b5cca3d4
renatalantos <renatalantos@gmail.com>
2021-09-24 11:04:45 +0000

    Create update functionality, still receiving errors

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
+
 db = SQLAlchemy(app)
 
 from taskmanager import routes 
